fb8927af915816f7a7343c8d1bf30c81c6a5789b
OrianeCrouzet <98615715+OrianeCrouzet@users.noreply.github.com>
2025-01-20 16:00:23 +0100

    Add CPS_fatima_oriane

---
+++ CPS_fatima_oriane/src/frontend/Facade.java
@@ -0,0 +1,39 @@
+package frontend;
+import java.io.Serializable;
+
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentDataI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentKeyI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.frontend.DHTServicesI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.CombinatorI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.ProcessorI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.ReductorI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.SelectorI;
+
+public class Facade implements DHTServicesI{
+
+	@Override
+	public ContentDataI get(ContentKeyI key) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ContentDataI put(ContentKeyI key, ContentDataI value) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ContentDataI remove(ContentKeyI key) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <R extends Serializable, A extends Serializable> A mapReduce(SelectorI selector, ProcessorI<R> processor,
+			ReductorI<A, R> reductor, CombinatorI<A> combinator, A initialAcc) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
